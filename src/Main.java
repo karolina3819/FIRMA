8bd1b0395bab84303acc1e0c80b4d309443b2c8b
STANOWISKO_D001 <karolina3819@gmail.com>
2022-03-13 12:51:52 +0100

    wersja0

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Pracownik> lista_aut=new ArrayList<>();
+        for (int i =0;i<3;i++){
+            lista_aut.add(new Pracownik());
+
+        }
+        for (Pracownik i : lista_aut){
+            System.out.println(i.toString());
+        }
+
+
+    }
+}
---
+++ src/Pracownik.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Pracownik {
+    private String Imię;
+    private String Nazwisko;
+    private int Wiek;
+    private int Płaca;
+    private String Adres;
+
+    public Pracownik() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("podaj Imię :");
+        this.Imię = input.next();
+        System.out.println("podaj Nazwisko:");
+        this.Nazwisko = input.next();
+        System.out.println("podaj Wiek:");
+        this.Wiek = Integer.parseInt(input.next());
+        System.out.println("podaj Płace:");
+        this.Płaca = Integer.parseInt(input.next());
+        System.out.println("podaj Adres:");
+        this.Adres = input.next();
+    }
+
+    public String getImię() {
+        return Imię;
+    }
+
+    public void setImię(String Imię) {
+        this.Imię = Imię;
+    }
+    public String getNazwisko() {
+        return Nazwisko;
+    }
+
+    public void setNazwisko(String Nazwisko) {
+        this.Nazwisko = Nazwisko;
+    }
+
+
+    public int getWiek() {
+        return Wiek;
+    }
+
+    public void setWiek(int Wiek) {
+        this.Wiek = Wiek;
+    }
+
+    public int getPłaca() {
+        return Płaca;
+    }
+
+    public void setPłaca(int Płaca) {
+        this.Płaca = Płaca;
+    }
+
+    public String toString() {
+        String tekst = "Imię to:" + Imię +"Nazwisko to:"+Nazwisko+"Płaca to:" + Płaca + " Wike to :" + Wiek +"Adres to:"+Adres;
+        return tekst;
+
+    }}
